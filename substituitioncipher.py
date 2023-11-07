64c0d6a234809cda2609ff55eee85e9cf2027294
Oskar Schiedewitz <116277974+naiylo@users.noreply.github.com>
2023-11-07 08:30:01 +0100

    Update substituitioncipher.py

--- substituitioncipher.py
+++ substituitioncipher.py
@@ -1,3 +1,35 @@
+# Substituition-Cipher
+
+# easy example that you could solve with Brute-Force
+
+dictionary = e = [(' ', 'P'), ('A', 'H'), ('B', 'K'), ('C', 'R'), ('D', 'Y'), ('E', 'X'), ('F', 'M'), ('G', 'G'), ('H', 'E'), ('I', 'Z'), ('J', 'W'), ('K', ' '), ('L', 'T'), ('M', 'U'), ('N', 'A'), ('O', 'F'), ('P', 'D'), ('Q', 'I'), ('R', 'O'), ('S', 'Q'), ('T', 'L'), ('U', 'S'), ('V', 'V'), ('W', '.'), ('X', 'B'), ('Y', 'C'), ('Z', 'N')] 
+example1Decoded = ""
+example1Encoded = "bsp3 = "PQHKBCD LRNZNTDUHKXHIHQYAZHLKQFKMCU CMHZGTQYAHZKUQZZHKPQHKULMPQHKVRZKFHLARPHZKMZPKLHYAZQBHZKMFKQZORCFNLQRZHZKNMUKVHCUYATMHUUHTLHZKLHELHZKIMKGHJQZZHZKPQHUHKQZORCFNLQRZHZKBRHZZHZKURJRATKPHCKVHCJHZPHLHKUYATMHUUHTKJQHKNMYAKPHCKRCQGQZNTLHELKUHQZKAHMLIMLNGHKXHIHQYAZHLKPHCKXHGCQOOKBCD LRNZNTDUHKNTTGHFHQZHCKPQHKNZNTDUHKVRZKBCD LRGCN AQUYAHZKVHCONACHZKFQLKPHFKIQHTKPQHUHKHZLJHPHCKIMKXCHYAHZKNTURKQACHKUYAMLIOMZBLQRZKNMOIMAHXHZKRPHCKQACHKUQYAHCAHQLKZNYAIMJHQUHZKMZPKIMKSMNZLQOQIQHCHZKBCD LRNZNTDUHKQULKPNFQLKPNUKGHGHZULMHYBKIMCKBCD LRGCN AQH"
+
+# help-functions
+
+# converts a string of capital letters to their fitting ASCII number
+def convertToNumber(string):
+    result = []
+    for c in string:
+        if c == " ":
+            result.append(26)
+        else:
+            result.append(ord(c)-65)
+    return result
+
+# converts a list of ASCII numbers to their fitting capital letters
+def convertToString(list):
+    result = ""
+    for c in list:
+        if c == 26:
+            result = result + ' '
+        else:
+            result = result + chr(c+65)
+    return result
+
+# decoding function
+
 def decodeSubstituitionCipher(c,e):
     result = ""
     for i in c:
@@ -11,7 +43,5 @@ def decodeSubstituitionCipher(c,e):
     print(result)
     print(e)
 
-
-bsp3 = "PQHKBCD LRNZNTDUHKXHIHQYAZHLKQFKMCU CMHZGTQYAHZKUQZZHKPQHKULMPQHKVRZKFHLARPHZKMZPKLHYAZQBHZKMFKQZORCFNLQRZHZKNMUKVHCUYATMHUUHTLHZKLHELHZKIMKGHJQZZHZKPQHUHKQZORCFNLQRZHZKBRHZZHZKURJRATKPHCKVHCJHZPHLHKUYATMHUUHTKJQHKNMYAKPHCKRCQGQZNTLHELKUHQZKAHMLIMLNGHKXHIHQYAZHLKPHCKXHGCQOOKBCD LRNZNTDUHKNTTGHFHQZHCKPQHKNZNTDUHKVRZKBCD LRGCN AQUYAHZKVHCONACHZKFQLKPHFKIQHTKPQHUHKHZLJHPHCKIMKXCHYAHZKNTURKQACHKUYAMLIOMZBLQRZKNMOIMAHXHZKRPHCKQACHKUQYAHCAHQLKZNYAIMJHQUHZKMZPKIMKSMNZLQOQIQHCHZKBCD LRNZNTDUHKQULKPNFQLKPNUKGHGHZULMHYBKIMCKBCD LRGCN AQH"
-e = [(' ', 'P'), ('A', 'H'), ('B', 'K'), ('C', 'R'), ('D', 'Y'), ('E', 'X'), ('F', 'M'), ('G', 'G'), ('H', 'E'), ('I', 'Z'), ('J', 'W'), ('K', ' '), ('L', 'T'), ('M', 'U'), ('N', 'A'), ('O', 'F'), ('P', 'D'), ('Q', 'I'), ('R', 'O'), ('S', 'Q'), ('T', 'L'), ('U', 'S'), ('V', 'V'), ('W', '.'), ('X', 'B'), ('Y', 'C'), ('Z', 'N')] 
+    
 okamelSim(bsp3,e)
