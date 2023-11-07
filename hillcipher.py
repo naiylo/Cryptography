dc445f5ae56d3c1962c41221685af0b1338e3b70
Oskar Schiedewitz <116277974+naiylo@users.noreply.github.com>
2023-11-07 12:53:21 +0100

    Update hillcipher.py

--- hillcipher.py
+++ hillcipher.py
@@ -6,6 +6,10 @@ wordthatappears = "CLASSICAL"
 example1Decoded = ""
 example1Encoded = "ZTGHFNATGBUCKAHMOPZOUAFAJYFUR OQYHYHNML NDE PQONFHSZCPDWZITEJSLOYWKGBEVZCTU HMGDRQKTQKAQFTRTCQBDOUYM"
 
+keyExample2 = [[11,8],[3,7]]
+example2Decoded = "ENDE UM ZWEI"
+example2Encoded = "CPSZWYVIRDOH"
+
 # help-functions
 
 # converts a string of capital letters to their fitting ASCII number
@@ -33,6 +37,19 @@ def convertToString(list):
 import numpy as np
 
 def encodeHillCipher(string, keymatrix):
-    result = []
+    shifted = []
     keymatrixinv = np.linalg.inv(keymatrix)
-    return result
+    basis = convertToNumber(string)
+    blocklength = keymatrix.shape[0]
+    for i in range(0, len(string), blocklength):
+        block = np.array(string[i:i+blocklength])
+        decrypted_block = (keymatrixinv.dot(block)) % 26
+        shifted += list(decrypted_block)
+    
+    # Konvertiere die Zahlen zurück in Buchstaben
+    plaintext = ''.join([chr(int(char) + ord('A')) for char in plaintext])
+    
+    return plaintext
+
+
+    
