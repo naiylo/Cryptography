8c87850c60e6b466e6dc7c1c1b7092641b948d2b
Oskar Schiedewitz <116277974+naiylo@users.noreply.github.com>
2023-11-08 00:06:42 +0100

    Update hillcipher.py

--- hillcipher.py
+++ hillcipher.py
@@ -4,16 +4,16 @@
 
 wordthatappears = "CLASSICAL"
 example1Decoded = ""
-example1Encoded = "TGHFNATGBUCKAHMOPZOUAFAJYFUR OQYHYHNML NDE  PQONFHSZCPDWZITEJSLOYWKGBEVZCTU  HMGDRQKTQKAQFTRTCQBDOUYM"
+example1Encoded = "ZTGHFNATGBUCKAHMOPZOUAFAJYFUR OQYHYHNML NDE  PQONFHSZCPDWZITEJSLOYWKGBEVZCTU  HMGDRQKTQKAQFTRTCQBDOUYM"
 
 keyExample2 = [[11,3],[8,7]]
 keyExample2inv = [[20,3],[8,16]]
 example2Decoded = "ENDE UM ZWEI"
 example2Encoded = "CPSZWYVIRDOH"
-
+  
 # help-functions
 
-# to make it as easy as possible we use some imports to simply some  functions
+# to make it as easy as possible we use numpy and math to multiply the matrizes
 
 import numpy as np
 import math as math
@@ -49,14 +49,25 @@ def modInverse(a, m):
 # gives us the inverse matrix (modulo is set to 27 because we have the capital letters A-Z and the blank space)
 def inverseMatrix(matrix, modulo = 27):
     matrix = np.array(matrix)
+    m, n = matrix.shape
     det = int(round(np.linalg.det(matrix)))
     det_inv = modInverse(det,modulo)
     
     if det == 0 or det_inv is None:
         # Matrix is singular or doesn't have a modular inverse
         return None
-    
-    adjugate = np.array([[matrix[1, 1], -matrix[0, 1]], [-matrix[1, 0], matrix[0, 0]]])
+
+    if m == n == 2:
+        adjugate = np.array([[matrix[1, 1], -matrix[0, 1]], [-matrix[1, 0], matrix[0, 0]]])
+
+    elif m == n == 3:
+        adjugate = np.zeros_like(matrix)
+
+        for i in range(3):
+            for j in range(3):
+                sub_matrix = np.delete(np.delete(matrix, i, 0), j, 1)
+                cofactor = int(round(np.linalg.det(sub_matrix)))
+                adjugate[i, j] = (-1) ** (i + j) * cofactor
     
     inverse_matrix = (det_inv * adjugate) % modulo
     
@@ -111,7 +122,7 @@ def decodeHillCipher(string, keymatrix):
 # if we have a encoded string and a word or sentence that we know is in the decoded string we can 
 # use some methods to compute the key matrix for that we need some help functions
 
-# function to determine if the found determinat of the found matrix ist coprime with his inverted matrix
+# function to determine if the found determinat of the found matrix is coprime with his inverted matrix
 # modulo is again set to 27 (same explanation as beforehand)
 
 def isCoprimeWithInverse(matrix, modulo=27):
@@ -147,42 +158,59 @@ def generateSubstrings(string, length):
 
     return substrings
 
-# futhermore we need a function to generate the key if I have a string know the decoded string
+# futhermore we need a function to generate the key if I have a string and I know the decoded string
 
 def findCipherKey(string, known, blocklength):
+
     basisstring = convertToNumber(string)
-    print(basisstring)
+    basisstring = np.array(basisstring)
+    basisstring = basisstring.reshape(blocklength,blocklength).T
+
+    print("basisstring:\n",basisstring)
+
     basisknown = convertToNumber(known)
-    print(basisknown)
+    basisknown = np.array(basisknown)
+    basisknown = basisknown.reshape(blocklength,blocklength).T
 
-    
-    a = (basisknown[0] - basisstring[0] * b) / basisstring[0]
-    b = (basisknown[0] - basisstring[0] * a) / basisstring[0]
-    c = (basisknown[1] - basisstring[1] * d) / basisstring[1]
-    d = (basisknown[1] - basisstring[1] * c) / basisstring[1]
+    print("basisknown:\n",basisknown)
 
-    possiblekey = [[a,b], [c,d]]
-    return possiblekey
+    P = inverseMatrix(basisknown, 27)
 
+    print("P:\n" ,P)
 
+    keymatrix = np.dot(basisstring, P) % 27
 
+    return inverseMatrix(keymatrix)
 
-print(convertToString(encodeHillCipher("ENDE", [[11,3],[8,7]])))
+# we can test this with our given example2
+#print(findCipherKey("ENDE", "CPSZ", 2))
 
-print(findCipherKey("ENDE", "CPSZ", 2))
-
-# now a function that gives us all possible substrings for the combinations
+# now a function that gives us all possible keys for all the combinations of substrings
 
 def findPossibleKeyMatrices(string, known, blocklength, modulo=27):
+
     possibleKeyMatrices = []
     length = len(known)
     substrings = generateSubstrings(string, length)
+    for i in substrings:
+        print("fitting on substring: ",i)
+        key = findCipherKey(i, known, blocklength)
+        print("the possible key,\n", key)
+        possibleKeyMatrices.append(key)
+        try:
+            print("decoded: ", decodeHillCipher(string, key))
+        except:
+            continue
 
-    
+    return possibleKeyMatrices
 
-    
+#print(findPossibleKeyMatrices(example2Encoded, "ENDE", 2))
 
-#print(findPossibleKeyMatrices(example2Decoded, "ENDE", 2))
+# now we try a harder example
 
+example3Encoded = "ZTGHFNATGBUCKAHMOPZOUAFAJYFUR OQYHYHNML NDE  PQONFHSZCPDWZITEJSLOYWKGBEVZCTU  HMGDRQKTQKAQFTRTCQBDOUYM"
+keyExample3size = 3
+wordthatappears = "CLASSICAL"
 
-    
+#findPossibleKeyMatrices(example2Encoded, "ENDE",2, modulo=27)
+findPossibleKeyMatrices(example3Encoded, wordthatappears,keyExample3size, modulo=27)
